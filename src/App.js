8fb89fcf5135cb418ead356abc9f9f39e6fd9644
younessyk <17533971+younessyk@users.noreply.github.com>
2018-08-30 18:04:52 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Clock.js';
+let time = new Date().toLocaleString();
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+         name {this.props.name}
+        </p>
+          <p>
+              date {time}
+          </p>
+          <Clock/>
+      </div>
+    );
+  }
+}
+
+export default App;
